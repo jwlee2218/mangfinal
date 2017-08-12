d32b5aca8bf7a1c4da34b6281ea4b935b6e521a8
jewoo <jwlee2218@gmail.com>
2017-08-12 19:02:43 +0900

    background 수정

--- mang/views.py
+++ mang/views.py
@@ -8,16 +8,18 @@ def background(request):
 		hits = post.hits
 		hits_list.append(hits)
 
-	most1hits = hits_list[0]
-	most2hits = hits_list[1]
-	if most1hits == most2hits:
-		filter_list = post_list.filter(hits = most1hits)
-		post1 = filter_list[0]
-		post2 = filter_list[1]
-	else:
-		post1 = Post.objects.get(hits = most1hits)
-		post2 = Post.objects.get(hits = most2hits)
-	return render(request, 'layout.html',{'post1':post1, 'post2':post2,})
+	post1=post_list[0]
+	post2=post_list[1]
+	# most1hits = hits_list[0]
+	# most2hits = hits_list[1]
+	# if most1hits == most2hits:
+	# 	filter_list = post_list.filter(hits = most1hits)
+	# 	post1 = filter_list[0]
+	# 	post2 = filter_list[1]
+	# else:
+	# 	post1 = Post.objects.get(hits = most1hits)
+	# 	post2 = Post.objects.get(hits = most2hits)
+	# return render(request, 'layout.html',{'post1':post1, 'post2':post2,})
 
 def introduce(request):
     return render(request, 'introduce.html')
