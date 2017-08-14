8f71efaea5f1ef74456b797ec7c23aae3810b499
jewoo <jwlee2218@gmail.com>
2017-08-14 21:22:42 +0900

    "python anywhere"

--- mang/views.py
+++ mang/views.py
@@ -13,8 +13,32 @@ def background(request):
 	try:
 		most1hits = hits_list[0]
 		most2hits = hits_list[1]
-		post1 = Post.objects.get(hits = most1hits)
-		post2 = Post.objects.get(hits = most2hits)
+		most3hits = hits_list[2]
+		if most1hits==most2hits:
+			date_list=[]
+			for post in Post.objects.get(hits=most1hits):
+				date=post.updated_at
+				date_list.append(date)
+			date_list.sort(reverse=True)
+			datesort1=date_list[0]
+			datesort2=date_list[1]
+			post1=Post.objects.get(hits=most1hits, date=datesort1)
+			post2=Post.objects.get(hits=most2hits, date=datesort2)
+
+		elif most3hits==most2hits and most1hits!=most2hits:
+			datelist=[]
+			for post in Post.objects.get(hits=most2hits):
+				date=post.updated_at
+				date_list.append(date)
+			date_list.sort(reverse=True)
+			datesort1=date_list[0]
+			datesort2=date_list[1]
+			post1=Post.objects.get(hits=mosthits)
+			post2=Post.objects.get(date=datesort1, hits=most2hits)
+
+		else:
+			post1 = Post.objects.get(hits = most1hits)
+			post2 = Post.objects.get(hits = most2hits)
 	except:
 		pass
 	return render(request, 'layout.html',{'post1':post1, 'post2':post2,})
